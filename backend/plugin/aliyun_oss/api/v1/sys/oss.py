023c08dc82291fecce3157ce9bcfb8fac2ec4e90
Wu Clan <jianhengwu0407@gmail.com>
2025-02-21 20:11:29 +0800

    Add oss plugin (#1)

---
+++ backend/plugin/aliyun_oss/api/v1/sys/oss.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from typing import Annotated
+
+from fastapi import APIRouter, File, UploadFile
+
+from backend.common.dataclasses import UploadUrl
+from backend.common.enums import FileType
+from backend.common.response.response_schema import ResponseSchemaModel, response_base
+from backend.common.security.jwt import DependsJwtAuth
+from backend.plugin.aliyun_oss.utils.file_ops import oss_put_object
+from backend.utils.file_ops import file_verify
+
+router = APIRouter()
+
+
+@router.post('/image', summary='上传图片', dependencies=[DependsJwtAuth])
+async def oss_image(file: Annotated[UploadFile, File()]) -> ResponseSchemaModel[UploadUrl]:
+    file_verify(file, FileType.image)
+    url = await oss_put_object(file)
+    return response_base.success(data={'url': url})
+
+
+@router.post('/video', summary='上传视频', dependencies=[DependsJwtAuth])
+async def oss_video(file: Annotated[UploadFile, File()]) -> ResponseSchemaModel[UploadUrl]:
+    file_verify(file, FileType.video)
+    url = await oss_put_object(file)
+    return response_base.success(data={'url': url})
---
+++ backend/plugin/aliyun_oss/utils/file_ops.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import oss2
+
+from asgiref.sync import sync_to_async
+from fastapi import UploadFile
+
+from backend.common.exception import errors
+from backend.common.log import log
+from backend.plugin.aliyun_oss.conf import oss_settings
+from backend.utils.file_ops import build_filename
+
+
+def get_oss_buket() -> oss2.Bucket:
+    """获取华为云 oss buket"""
+    auth = oss2.Auth(oss_settings.OSS_ACCESS_KEY, oss_settings.OSS_SECRET_KEY)
+    buket = oss2.Bucket(auth, oss_settings.OSS_ENDPOINT, oss_settings.OSS_BUCKET_NAME)
+    return buket
+
+
+@sync_to_async
+def oss_put_object(file: UploadFile) -> str:
+    """
+    oss 简单上传（bytes）
+
+    :param file:
+    :return:
+    """
+    filename = build_filename(file)
+    buket = get_oss_buket()
+    try:
+        res = buket.put_object(filename, file.file)
+    except Exception as e:
+        log.error(f'上传文件 {filename} 失败：{str(e)}')
+        raise errors.RequestError(msg='上传文件失败')
+    return res.resp.response.url
