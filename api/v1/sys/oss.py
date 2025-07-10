4d7f2c5bbecace9b444c34d4dc08e3285984483f
Wu Clan <jianhengwu0407@gmail.com>
2025-07-10 20:18:23 +0800

    Fix the file verification method

--- api/v1/sys/oss.py
+++ api/v1/sys/oss.py
@@ -5,24 +5,23 @@ from typing import Annotated
 from fastapi import APIRouter, File, UploadFile
 
 from backend.common.dataclasses import UploadUrl
-from backend.common.enums import FileType
 from backend.common.response.response_schema import ResponseSchemaModel, response_base
 from backend.common.security.jwt import DependsJwtAuth
 from backend.plugin.aliyun_oss.utils.file_ops import oss_put_object
-from backend.utils.file_ops import file_verify
+from backend.utils.file_ops import upload_file_verify
 
 router = APIRouter()
 
 
 @router.post('/image', summary='上传图片', dependencies=[DependsJwtAuth])
 async def oss_image(file: Annotated[UploadFile, File()]) -> ResponseSchemaModel[UploadUrl]:
-    file_verify(file, FileType.image)
+    upload_file_verify(file)
     url = await oss_put_object(file)
     return response_base.success(data={'url': url})
 
 
 @router.post('/video', summary='上传视频', dependencies=[DependsJwtAuth])
 async def oss_video(file: Annotated[UploadFile, File()]) -> ResponseSchemaModel[UploadUrl]:
-    file_verify(file, FileType.video)
+    upload_file_verify(file)
     url = await oss_put_object(file)
     return response_base.success(data={'url': url})
